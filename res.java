e38238a530e069b667f59f1ccc733b227cc314e2
Aditya pratap singh <72407189+roxy-jr@users.noreply.github.com>
2020-12-05 21:39:18 +0530

    Add files via upload

---
+++ res.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class mortal
+{
+public static void main(String []e)
+{
+	int i;
+System.out.println("enter the size of array");
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int ar[]=new int[n];
+System.out.println("enter the"+ n +" elements of array");
+for(i=0;i<n;i++)
+{
+ar[i]=sc.nextInt();
+}
+for(int i=0;i<=n-1;i++){
+    br[i]=ar[i];
+}
