4525f82a67a88bd6eef2704d5967ea1d0c5ba904
Aditya pratap singh <72407189+roxy-jr@users.noreply.github.com>
2021-01-21 07:59:41 -0800

    Add files via upload

---
+++ staticf.java
@@ -0,0 +1,7 @@
+public class staticf{
+    public static void main(String[]gh){
+     int a=10;
+       // static int a=10; we can not create static variable inside a method
+        System.out.println(a);
+    }
+}
